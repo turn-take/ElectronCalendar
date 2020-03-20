9f2a4f7634a2704430c0a43eb56a65d8ce8495cd
h.takei <take.mh.imagin-lv@docomo.ne.jp>
2020-03-20 23:47:38 +0900

    機能追加

--- js/index.js
+++ js/index.js
@@ -3,14 +3,35 @@
 const moment = require("moment");
 
 window.onload = function() {
+    createCallendar(moment());
+
+    //先月ボタン押下時
+    document.getElementById('preMonth').onclick = function() {
+        createCallendar(moment(this.value));
+    }
+
+    //来月ボタン押下時
+    document.getElementById('nextMonth').onclick = function() {
+        createCallendar(moment(this.value));
+    }
+}
+
+/**
+ * カレンダーを表示する。
+ * @param momentオブジェクト 
+ */
+const createCallendar = function(localMoment) {
     //captionを表示
-    document.getElementById('caption').innerText = moment().format("YYYY年MM月");
+    document.getElementById('caption').innerText = localMoment.format("YYYY年MM月");
     
+    //カレンダー初期化
+    clearCallendar();
+
     //当月の日数を取得
-    const daysOfMonth = moment().daysInMonth();
+    const daysOfMonth = localMoment.daysInMonth();
 
     //月初の曜日を取得（index.htmlと合わせるために＋1する）
-    const firstDayOfManth = moment().startOf('month').day() + 1;
+    const firstDayOfManth = localMoment.startOf('month').day() + 1;
 
     //カレンダーの各セルに日付を表示させる
     let cellIndex = 0;
@@ -21,5 +42,28 @@ window.onload = function() {
             cellIndex++;
         }
         document.getElementById("cell" + cellIndex).innerText = i;
+    }
+
+    //6行目の第1セルが空白なら6行目自体を非表示にする。
+    if(document.getElementById("cell36").innerText === "") {
+        document.getElementById('row6').style.visibility = "hidden";
+    }
+
+    //先月
+    document.getElementById('preMonth').value = localMoment.add(-1,'month').format("YYYY-MM");
+    //来月(先月のmomentオブジェクトとなっているので＋2ヶ月)
+    document.getElementById('nextMonth').value = localMoment.add(2,'month').format("YYYY-MM");
+}
+
+/**
+ * カレンダーを初期化する
+ */
+const clearCallendar = function() {
+    //6行目を表示させておく
+    if(document.getElementById('row6').style.visibility === "hidden") {
+        document.getElementById('row6').style.visibility = "visible";
+    }
+    for(let i = 1; i < 43; i++) {
+        document.getElementById("cell" + i).innerText = "";
     }
 }
--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.on('window-all-closed', function() {
 // Electronの初期化完了後に実行
 app.on('ready', function() {
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 650});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // ウィンドウが閉じられたらアプリも終了
